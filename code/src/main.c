ef2e695e36071209958a23423119c14221251645
Kyle <k13bernier@gmail.com>
2018-05-27 20:50:04 -0400

    Switched code to use macros

--- code/src/main.c
+++ code/src/main.c
@@ -1,38 +1,49 @@
+/**
+ * @file main.c
+ * @author Kyle Bernier and Daeghan Elkin
+ * @date 2018 May 27
+ * 
+ * @brief Blinks an led at the moment. Using macros to
+ *        increase readability.
+ *
+ */
+
 #include "stm32l4xx.h"
 
-void delay(volatile unsigned dly)
+/* Simple delay, will use systick at some point */
+void delay(volatile unsigned delay)
 {
-    while (dly--);
+	while (delay--);
 }
 
-int main()
+/* Main function */
+int main(void)
 {
-	// Enable GPIOB and GPIOE
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN 
-		+ RCC_AHB2ENR_GPIOEEN;
+	// Enable GPIOB
+	SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOBEN);
+	// Enable GPIOE
+	SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOEEN);
 
 	// Set GPIOB_2 and GPIOE_8 as outputs
-	GPIOB->MODER |= GPIO_MODER_MODE2_0;
-	GPIOB->MODER &= ~GPIO_MODER_MODE2_1;
-	GPIOE->MODER |= GPIO_MODER_MODE8_0;
-	GPIOE->MODER &= ~GPIO_MODER_MODE8_1;
+	MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODE2, GPIO_MODER_MODE2_0);
+	MODIFY_REG(GPIOE->MODER, GPIO_MODER_MODE8, GPIO_MODER_MODE8_0);
 
 	// Alternate blinking of two leds
 	while (1) {
 		// Turn on read led
-		GPIOB->ODR |= GPIO_ODR_OD2;
+		SET_BIT(GPIOB->ODR, GPIO_ODR_OD2);
 		// Turn off green led
-		GPIOE->ODR &= ~GPIO_ODR_OD8;
+		CLEAR_BIT(GPIOE->ODR, GPIO_ODR_OD8);
 		
-		// Delay
+		// Delay a bit
 		delay(200000);
 		
 		// Turn off red led
-		GPIOB->ODR &= ~GPIO_ODR_OD2;
+		CLEAR_BIT(GPIOB->ODR, GPIO_ODR_OD2);
 		// Turn on green led
-		GPIOE->ODR |= GPIO_ODR_OD8;
+		SET_BIT(GPIOE->ODR, GPIO_ODR_OD8);
 
-		// Delay
+		// Delay a bit
 		delay(200000);
 	}
 }
